6325f33646b76d41e66512a32a9accec0aa5d223
rakesh mishra <rakesh.e.mishra@accenture.com>
2020-02-04 22:50:57 +0530

    Initial Commit

---
+++ ViewPagerLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewPagerLayout
+//
+//  Created by rakesh.e.mishra on 04/02/20.
+//  Copyright © 2020 rakesh.e.mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
